dda82af4a3996f49fa3324a971aafcb38b5ed854
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-08-07 16:58:03 -0400

    trying to understand yup oh my god

--- my-app/src/components/FormComponent.js
+++ my-app/src/components/FormComponent.js
@@ -4,13 +4,53 @@ import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 import './FormComponent.css'
 
-function FormComponent() {
+function FormComponent({ errors, touched, values, handleSubmit, status }) {
+  const [coolForm, setCoolForm] = useState([])
+  console.log(coolForm);
+
+  useEffect(() => {
+    if (status) {
+      setCoolForm([...coolForm, status]);
+    }
+  }, [status]);
+
   return(
     <div className="Form">
       <h2>Sign Up</h2>
+      <Form>
+        <Field type="text" name="name" placeholder="Name" />
 
+
+        <Field
+
+        />
+
+        <Field
+
+        />
+      </Form>
     </div>
   )
 }
+
+const FormikFormComponent = withFormik({
+  mapPropsToValues({ name, email, password, tos }) {
+    return {
+        name: name || '',
+        email: email || '',
+        password: password || '',
+        tos: tos || false
+    };
+  },
+
+  handleSubmit(values, { setStatus }) {
+    axios
+      .post('https://reqres.in/api/users')
+      .then(res => {
+        setStatus(res.data);
+      })
+      .catch(err => console.log(err.response))
+  }
+})
 
 export default FormComponent;
